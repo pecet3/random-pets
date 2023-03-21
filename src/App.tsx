e7d52246c88132d5639761f61a1e29892fc5aaa4
Jakub Pacewicz <pecet3107@gmail.coim>
2023-03-21 18:43:24 +0100

    edited div

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import { Header } from "./Header";
-import { Sidebar } from "./Sidebar";
+import { Header } from "./common/Header";
+import { Sidebar } from "./common/Sidebar/Sidebar";
 function App() {
   return (
     <>
       <div className="">
         <div className="bg-gray-200 dark:bg-slate-600">
-          <div className="max-w-full m-auto my-0 text-center pb-[100%]">
+          <div className="m-auto my-0 max-w-full pb-[100%] text-center">
             <Header title={"Random Pets"} />
             <Sidebar />
           </div>
