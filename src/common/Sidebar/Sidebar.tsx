f6cbc65f92b77abd982393053bae48f3354dd661
Jakub Pacewicz <pecet3107@gmail.coim>
2023-03-21 19:31:08 +0100

    added Sidebar

--- src/common/Sidebar/Sidebar.tsx
+++ src/common/Sidebar/Sidebar.tsx
@@ -1,11 +1,20 @@
+import { RiInformationFill } from "react-icons/ri";
+import { TbDog, TbCat } from "react-icons/tb";
+interface IIcon {
+  icon: JSX.Element;
+}
+
 export const Sidebar: React.FC = () => {
   return (
-    <div className="fixed top-0 flex w-16 flex-col bg-gray-700 shadow-2xl shadow-slate-900">
-      <i className="sidebar-icon">A</i>
-      <i>B</i>
-      <i>C</i>
-    </div>
+    <>
+      <div className="fixed top-0 flex w-16 flex-col gap-2 bg-gray-700 py-8 shadow-2xl shadow-slate-900">
+        <Icon icon={<TbDog />} />
+        <Icon icon={<TbCat />} />
+        <Icon icon={<RiInformationFill />} />
+      </div>
+    </>
   );
 };
-
-export const SideBarIcon;
+const Icon: React.FC<IIcon> = ({ icon }) => {
+  return <div className="sidebar-icon text-blue-400">{icon}</div>;
+};
