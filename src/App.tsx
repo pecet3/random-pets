0c42347b0d2ba63edc7f7f4a457406c53836c1af
Jakub Pacewicz <pecet3107@gmail.coim>
2023-03-21 18:12:30 +0100

    styled div

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,9 @@ function App() {
   return (
     <>
       <div className="">
-        <div className="bg-purple-400 dark:bg-slate-600">
-          <div className="max-w-full m-auto my-0 text-center">
-            <Header />
+        <div className="bg-gray-200 dark:bg-slate-600">
+          <div className="max-w-full m-auto my-0 text-center pb-[100%]">
+            <Header title={"Random Pets"} />
           </div>
         </div>
       </div>
