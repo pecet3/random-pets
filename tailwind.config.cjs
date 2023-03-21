ebda922d6a2a76fb375436862b006126a5ccd990
Jakub Pacewicz <pecet3107@gmail.coim>
2023-03-21 17:45:31 +0100

    removed colors, added darkMode

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,23 +4,13 @@ module.exports = {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
+  darkMode: 'class',
   theme: {
     screens: {
       sm: '480px',
       md: '768px',
       lg: '976px',
       xl: '1440px',
-    },
-    colors: {
-      'blue': '#1fb6ff',
-      'purple': '#7e5bef',
-      'pink': '#ff49db',
-      'orange': '#ff7849',
-      'green': '#13ce66',
-      'yellow': '#ffc82c',
-      'gray-dark': '#273444',
-      'gray': '#8492a6',
-      'gray-light': '#d3dce6',
     },
     fontFamily: {
       sans: ['Graphik', 'sans-serif'],
