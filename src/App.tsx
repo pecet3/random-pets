4893188569d35873ac600f238b9f599997b4ca70
Jakub Pacewicz <pecet3107@gmail.coim>
2023-03-21 17:45:18 +0100

    edited App

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
-import "./App.css";
-
+import { Header } from "./Header";
 function App() {
-  return <></>;
+  return (
+    <>
+      <div className="">
+        <div className="bg-purple-400 dark:bg-slate-600">
+          <div className="max-w-full m-auto my-0 text-center">
+            <Header />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
