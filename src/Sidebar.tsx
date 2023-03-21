d7da627a2bd0e691b9872899a4e5d696011cafd7
Jakub Pacewicz <pecet3107@gmail.coim>
2023-03-21 18:43:45 +0100

    replaced the files

--- src/Sidebar.tsx
+++
@@ -1,9 +0,0 @@
-export const Sidebar: React.FC = () => {
-  return (
-    <div className="fixed top-0 w-16 flex flex-col shadow-2xl shadow-slate-900 bg-gray-700">
-      <i>A</i>
-      <i>B</i>
-      <i>C</i>
-    </div>
-  );
-};
---
+++ src/common/Sidebar/Sidebar.tsx
@@ -0,0 +1,11 @@
+export const Sidebar: React.FC = () => {
+  return (
+    <div className="fixed top-0 flex w-16 flex-col bg-gray-700 shadow-2xl shadow-slate-900">
+      <i className="sidebar-icon">A</i>
+      <i>B</i>
+      <i>C</i>
+    </div>
+  );
+};
+
+export const SideBarIcon;
