c5508ad813c3f3e9e278ea733e8f609d4910c4ba
Jakub Pacewicz <pecet3107@gmail.coim>
2023-03-22 20:02:29 +0100

    implemented react-router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,30 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Header } from "./common/Header";
+import { Sidebar } from "./common/Sidebar";
+import { Dogs } from "./features/Dogs";
+import App from "./App";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/dogs",
+    element: <Dogs />,
+  },
+  {
+    path: "/header",
+    element: <Header title={"pieski"} />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
+    <RouterProvider router={router} />
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
