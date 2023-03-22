ac6132346ca54fb869efdc7bcb1e62ca3f17573f
Jakub Pacewicz <pecet3107@gmail.coim>
2023-03-22 20:17:16 +0100

    new component

---
+++ src/features/Cats.tsx
@@ -0,0 +1,10 @@
+import { Sidebar } from ".././common/Sidebar";
+import { Header } from ".././common/Header";
+export const Cats = () => {
+  return (
+    <>
+      <Sidebar />
+      <Header title="Random Cats" />
+    </>
+  );
+};
