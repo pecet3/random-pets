3092b0973982c1b6cd4ab092e2eb8f56f2336414
Jakub Pacewicz <pecet3107@gmail.coim>
2023-03-27 23:08:19 +0200

    removed getDogs

--- src/features/Dogs/Dogs.tsx
+++ src/features/Dogs/Dogs.tsx
@@ -2,24 +2,13 @@ import { Sidebar } from "../../common/Sidebar";
 import { Header } from "../../common/Header";
 import { useEffect, useState } from "react";
 import { TResponse } from "./models";
-import axios from "axios";
+import { getDogs } from "./getDogs";
 
 export const Dogs: React.FC = () => {
   const [response, setResponse] = useState<TResponse>();
 
-  const getDogs = async () => {
-    try {
-      const response = await axios.get(
-        "https://dog.ceo/api/breeds/image/random"
-      );
-      setResponse(response.data);
-    } catch (err) {
-      console.log(err);
-    }
-  };
-
   useEffect(() => {
-    getDogs();
+    getDogs(setResponse);
   }, []);
 
   return (
