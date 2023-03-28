0b920b35226e6056a5a931ff8ac869b2851f5254
Jakub Pacewicz <pecet3107@gmail.coim>
2023-03-29 00:19:12 +0200

    added padding to header

--- src/common/Header.tsx
+++ src/common/Header.tsx
@@ -6,7 +6,7 @@ interface IHeader {
 
 export const Header: React.FC<IHeader> = ({ title }) => {
   return (
-    <header className="m2">
+    <header className="p-5">
       <h1 className="font-comics text-4xl text-green-700">{title}</h1>
     </header>
   );
