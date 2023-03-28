0c94f936b4cca9ebbde8320fc61901cef028dc21
Jakub Pacewicz <pecet3107@gmail.coim>
2023-03-29 00:19:00 +0200

    removed Sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { RouterProvider } from "react-router-dom";
-import { Sidebar } from "./common/Sidebar";
 import { router } from "./main";
 
 function App() {
@@ -9,7 +8,6 @@ function App() {
         <div className="bg-gray-200 dark:bg-slate-600">
           <div className="m-auto my-0 max-w-full pb-[100%] text-center">
             <RouterProvider router={router} />
-            <Sidebar />
           </div>
         </div>
       </div>
