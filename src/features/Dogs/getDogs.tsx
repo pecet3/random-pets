ba6f55c68f4b14ad336c1e2523beaec82130ac59
Jakub Pacewicz <pecet3107@gmail.coim>
2023-03-28 23:49:50 +0200

    removed undefined from type

--- src/features/Dogs/getDogs.tsx
+++ src/features/Dogs/getDogs.tsx
@@ -3,11 +3,11 @@ import { Dispatch, SetStateAction } from "react";
 import { TResponse } from "./models";
 
 interface IFetch {
-  setResponse: Dispatch<SetStateAction<TResponse | undefined>>;
+  setResponse: Dispatch<SetStateAction<TResponse>>;
 }
 export const getDogs = async ({ setResponse }: IFetch) => {
   try {
-    const response = await axios.get("https://dog.ceo/api/breeds/image/random");
+    const response = await axios.get("https://dog.ceo/api/breeds/iage/random");
     setResponse(response.data);
   } catch {
     setResponse({
