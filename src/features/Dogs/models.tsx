30cf60880d931d706aaff7f7a847bd335884d979
Jakub Pacewicz <pecet3107@gmail.coim>
2023-03-29 23:59:30 +0200

    removed the file

--- src/features/Dogs/models.tsx
+++
@@ -1,9 +0,0 @@
-import { Dispatch, SetStateAction } from "react";
-export type TResponse = {
-  message: string;
-  status: string;
-};
-
-export interface IFetch {
-  setResponse: Dispatch<SetStateAction<TResponse>>;
-}
