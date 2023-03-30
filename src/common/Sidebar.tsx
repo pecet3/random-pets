397928d8e911dec4b1e89f462c1ff186787156de
Jakub Pacewicz <pecet3107@gmail.coim>
2023-03-30 23:58:47 +0200

    replaced Icon to the spearate file

--- src/common/Sidebar.tsx
+++ src/common/Sidebar.tsx
@@ -1,22 +1,19 @@
 import { RiInformationFill } from "react-icons/ri";
 import { TbDog, TbCat } from "react-icons/tb";
-import { IIcon } from "./models";
+import { Icon } from "./Icon";
 
 export const Sidebar: React.FC = () => {
   return (
     <>
       <nav className="fixed top-0 my-16 flex w-16 flex-col gap-2 rounded-r-md bg-gray-700 py-8 shadow-2xl shadow-slate-900">
         <a href="/dogs">
-          <Icon icon={<TbDog size="28" />} />
+          <Icon icon={<TbDog size="28" />} classes="sidebar-icon" />
         </a>
         <a href="/cats">
-          <Icon icon={<TbCat size="28" />} />
+          <Icon icon={<TbCat size="28" />} classes="sidebar-icon" />
         </a>
-        <Icon icon={<RiInformationFill size="28" />} />
+        <Icon icon={<RiInformationFill size="28" />} classes="sidebar-icon" />
       </nav>
     </>
   );
-};
-const Icon: React.FC<IIcon> = ({ icon }) => {
-  return <div className="sidebar-icon">{icon}</div>;
 };
