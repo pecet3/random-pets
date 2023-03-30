653e2471db117109c6de8ed024634d494f0ca20d
Jakub Pacewicz <pecet3107@gmail.coim>
2023-03-30 22:48:44 +0200

    added type to axios.get

--- src/features/Dogs/getDogs.tsx
+++ src/features/Dogs/getDogs.tsx
@@ -1,9 +1,11 @@
 import axios from "axios";
-import { IFetch } from "../../common/models";
+import { IFetch, TResponse } from "../../common/models";
 
 export const getDogs = async ({ setResponse }: IFetch) => {
   try {
-    const response = await axios.get("https://dog.ceo/api/breeds/image/random");
+    const response = await axios.get<TResponse>(
+      "https://dog.ceo/api/breeds/image/random"
+    );
     setResponse(response.data);
   } catch {
     setResponse({
