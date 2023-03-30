ebe512128c61be445a3e09251b5c946ee8e35979
Jakub Pacewicz <pecet3107@gmail.coim>
2023-03-30 23:58:24 +0200

    styled elements

--- src/common/RefreshButton.tsx
+++ src/common/RefreshButton.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { IFetch, TResponse } from "./models";
+import { Icon } from "./Icon";
+import { TbDog, TbCat } from "react-icons/tb";
 interface IRefresh {
   action: () => void;
 }
@@ -10,10 +11,13 @@ export const RefreshButton: React.FC<IRefresh> = ({ action }) => {
   return (
     <div>
       <button
-        className="mt-2 rounded-lg bg-gray-400 p-2"
+        className="mt-2 rounded-lg bg-gray-400 p-2 transition-all duration-300 hover:scale-[1.015] hover:bg-gray-500"
         onClick={onClickButton}
       >
-        get new pet
+        <div className="flex-raw flex justify-center gap-2">
+          <Icon icon={<TbDog size="28" />} classes="mx-auto" />
+          get new pet
+        </div>
       </button>
     </div>
   );
