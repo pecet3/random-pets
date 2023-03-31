d9bb5b512ea45d47d870d2716003051d37f97e4f
Jakub Pacewicz <pecet3107@gmail.coim>
2023-03-31 19:57:58 +0200

    styled h1

--- src/common/Header.tsx
+++ src/common/Header.tsx
@@ -7,7 +7,9 @@ interface IHeader {
 export const Header: React.FC<IHeader> = ({ title }) => {
   return (
     <header className="p-2">
-      <h1 className="font-comics text-4xl text-green-700">{title}</h1>
+      <h1 className="font-comics text-2xl text-green-700 lg:text-4xl">
+        {title}
+      </h1>
     </header>
   );
 };
