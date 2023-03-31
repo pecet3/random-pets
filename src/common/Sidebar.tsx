3ff19323f29339ca3dd20ea7272cdd92173b51fc
Jakub Pacewicz <pecet3107@gmail.coim>
2023-03-31 18:36:25 +0200

    className - sidebar

--- src/common/Sidebar.tsx
+++ src/common/Sidebar.tsx
@@ -5,7 +5,7 @@ import { Icon } from "./Icon";
 export const Sidebar: React.FC = () => {
   return (
     <>
-      <nav className="fixed top-0 my-16 flex w-16 flex-col gap-2 rounded-r-md bg-gray-700 py-8 shadow-2xl shadow-slate-900">
+      <nav className="sidebar">
         <a href="/dogs">
           <Icon icon={<TbDog size="28" />} classes="sidebar-icon" />
         </a>
