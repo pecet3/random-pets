16ed066865f5bcbf98234c2b24b6a823e8616676
Jakub Pacewicz <pecet3107@gmail.coim>
2023-03-31 20:32:36 +0200

    added img

--- src/features/Cats.tsx
+++ src/features/Cats.tsx
@@ -4,6 +4,10 @@ export const Cats = () => {
   return (
     <>
       <Header title="Random Cats" />
+      <img
+        src="https://st3.depositphotos.com/5482604/12908/i/950/depositphotos_129088418-stock-photo-builder-cat-in-helmet.jpg"
+        alt="a random cat"
+      />
     </>
   );
 };
