f3a41e81f331038015d3e47c5ec000be395a7f53
Jakub Pacewicz <pecet3107@gmail.coim>
2023-04-03 14:04:57 +0200

    changed padding

--- src/common/RefreshButton.tsx
+++ src/common/RefreshButton.tsx
@@ -11,7 +11,7 @@ export const RefreshButton: React.FC<IRefresh> = ({ action }) => {
   return (
     <div>
       <button
-        className="mt-2 rounded-xl bg-teal-300 p-2 transition-all duration-300 hover:rounded-md hover:bg-teal-400"
+        className="mt-2 rounded-xl bg-teal-300 p-3 transition-all duration-300 hover:rounded-md hover:bg-teal-400"
         onClick={onClickButton}
       >
         <div className="flex-raw flex justify-center gap-2">
