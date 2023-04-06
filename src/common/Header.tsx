50a11f554945ffac40e52997e5948adb910ded65
Jakub Pacewicz <pecet3107@gmail.coim>
2023-04-06 19:08:56 +0200

    updated tailwind h1

--- src/common/Header.tsx
+++ src/common/Header.tsx
@@ -7,7 +7,7 @@ interface IHeader {
 export const Header: React.FC<IHeader> = ({ title }) => {
   return (
     <header className="p-2 pb-4 drop-shadow-md md:p-4 lg:bg-transparent">
-      <h1 className="font-comics text-4xl text-green-700 drop-shadow-md lg:text-5xl ">
+      <h1 className="font-comics text-4xl text-green-700 drop-shadow-md dark:text-green-500 lg:text-5xl ">
         {title}
       </h1>
     </header>
