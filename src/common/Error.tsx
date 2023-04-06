9e58f79c9cda227ba4ea1b0214ff0d34c695ac18
Jakub Pacewicz <pecet3107@gmail.coim>
2023-04-06 19:08:39 +0200

    crerated the component

---
+++ src/common/Error.tsx
@@ -0,0 +1,8 @@
+export const Error = () => {
+  return (
+    <div className="h-[324px] w-auto pt-16 md:h-[520px]">
+      <p className="font-comics text-2xl">error 😪</p>
+      <p className="text-1xl mt-2">please refresh the site</p>
+    </div>
+  );
+};
