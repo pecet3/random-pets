2b34d61c45f5f75e557c9b06663095fdad450dc0
Jakub Pacewicz <pecet3107@gmail.coim>
2023-04-06 19:08:45 +0200

    created the component

---
+++ src/common/Loading.tsx
@@ -0,0 +1,8 @@
+export const Loading = () => {
+  return (
+    <div className="h-[324px] w-auto pt-16 md:h-[520px]">
+      <p className="font-comics text-2xl">loading...</p>
+      <p className="text-1xl mt-2">please be patient 🙄</p>
+    </div>
+  );
+};
