1df6cfdc604b08495f070e975aa68255eda4b8b2
Jakub Pacewicz <pecet3107@gmail.coim>
2023-04-06 04:02:39 +0200

    added new param

--- src/common/RefreshButton.tsx
+++ src/common/RefreshButton.tsx
@@ -3,20 +3,21 @@ import { Icon } from "./Icon";
 import { TbRefresh } from "react-icons/tb";
 interface IRefresh {
   action: () => void;
+  petType?: string;
 }
-export const RefreshButton: React.FC<IRefresh> = ({ action }) => {
+export const RefreshButton: React.FC<IRefresh> = ({ action, petType }) => {
   const onClickButton = () => {
     action();
   };
   return (
     <div>
       <button
-        className="mt-2 rounded-xl bg-teal-300 p-3 transition-all duration-300 hover:rounded-md hover:bg-teal-400"
+        className="mt-2 rounded-xl bg-teal-300 p-3 transition-all duration-300 md:hover:rounded-md md:hover:bg-teal-400"
         onClick={onClickButton}
       >
         <div className="flex-raw flex justify-center gap-2">
           <Icon icon={<TbRefresh size="24" />} classes="mx-auto" />
-          get new pet
+          <p className="font-comics">New {petType}</p>
         </div>
       </button>
     </div>
