29cf122ba7a7e302e5262c9389555f669f9847c2
Jakub Pacewicz <pecet3107@gmail.coim>
2023-04-06 17:19:15 +0200

    created the custom hook

---
+++ src/common/useTheme.tsx
@@ -0,0 +1,22 @@
+import { useState, useEffect } from "react";
+
+type Theme = "light" | "dark";
+
+const useTheme = (): { theme: Theme; toggleTheme: () => void } => {
+  const [theme, setTheme] = useState<Theme>(
+    (localStorage.getItem("theme") as Theme) || "light"
+  );
+
+  useEffect(() => {
+    document.documentElement.setAttribute("data-theme", theme);
+    localStorage.setItem("theme", theme);
+  }, [theme]);
+
+  const toggleTheme = () => {
+    setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
+  };
+
+  return { theme, toggleTheme };
+};
+
+export default useTheme;
