fe119a0b82f25923f560a0e906427732c5936018
Jakub Pacewicz <pecet3107@gmail.coim>
2023-04-06 19:14:54 +0200

    requirments to display proper icon, updated tailwind

--- src/common/ToggleThemeButton.tsx
+++ src/common/ToggleThemeButton.tsx
@@ -1,6 +1,4 @@
-import { RiInformationFill } from "react-icons/ri";
-import { TbDog, TbCat, TbMoon } from "react-icons/tb";
-import { Link } from "react-router-dom";
+import { TbMoon, TbSun } from "react-icons/tb";
 import { Icon } from "./Icon";
 
 export interface IToggle {
@@ -10,16 +8,19 @@ export interface IToggle {
 export const ToggleThemeButton: React.FC<IToggle> = ({ action, state }) => {
   return (
     <button className="group" onClick={action}>
-      <Icon icon={<TbMoon size="28" />} classes="sidebar-icon" />
+      <Icon
+        icon={state === "dark" ? <TbSun size="28" /> : <TbMoon size="28" />}
+        classes="sidebar-icon"
+      />
       <div
-        className="absolute hidden w-20 rounded-r-md rounded-br-md border-2 border-transparent 
+        className="absolute hidden w-24 rounded-r-md rounded-br-md border-2 border-transparent 
         text-sm text-transparent
         transition-colors duration-300
         group-hover:visible
       group-hover:bg-gray-600 group-hover:text-emerald-500
         lg:invisible lg:left-16 lg:bottom-[34px] lg:block"
       >
-        Change to darkmode
+        Change to {state && state[0].toUpperCase() + state.slice(1)} mode
       </div>
     </button>
   );
