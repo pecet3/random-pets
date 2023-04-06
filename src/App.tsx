b63c834eee204bb9a8fc5d8b7b30157f3d45a07e
Jakub Pacewicz <pecet3107@gmail.coim>
2023-04-06 19:55:41 +0200

    updated tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { router } from "./main";
 function App() {
   return (
     <div>
-      <div className="bg-gray-300 dark:bg-slate-500">
+      <div className="bg-[url('./lightBgImage.jpg')] from-gray-800 to-pink-900 dark:bg-gradient-to-br dark:text-slate-200">
         <div className="m-auto my-0 max-w-full pb-[100%] text-center ">
           <RouterProvider router={router} />
         </div>
