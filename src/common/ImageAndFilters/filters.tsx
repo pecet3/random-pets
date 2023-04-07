444fd1a063057fb6e112d1abd244934964f6efc4
Jakub Pacewicz <pecet3107@gmail.coim>
2023-04-07 13:27:45 +0200

    created array of objects

---
+++ src/common/ImageAndFilters/filters.tsx
@@ -0,0 +1,18 @@
+export const filters: { name: string; value: string }[] = [
+  {
+    name: "Sepia",
+    value: "sepia",
+  },
+  {
+    name: "None",
+    value: "",
+  },
+  {
+    name: "Invert",
+    value: "invert",
+  },
+  {
+    name: "GrayScale",
+    value: "grayscale",
+  },
+];
