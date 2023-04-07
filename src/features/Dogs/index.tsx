fe28ca2aa1595b9cd0a5507a6029cb3c2d3c27c7
Jakub Pacewicz <pecet3107@gmail.coim>
2023-04-07 15:04:44 +0200

    implemented useGetDogs

--- src/features/Dogs/index.tsx
+++ src/features/Dogs/index.tsx
@@ -1,23 +1,13 @@
 import { RefreshButton } from "../../common/RefreshButton";
 import { Header } from "../../common/Header";
-import { useEffect, useState } from "react";
-import { TResponse } from "../../common/models";
 import { Sidebar } from "../../common/Sidebar";
-import { getDogs } from "./getDogs";
 import { Error } from "../../common/Error";
 import { Loading } from "../../common/Loading";
 import { MainWrapper } from "../../common/MainWrapper";
 import { ImageAndFilters } from "../../common/ImageAndFilters";
+import { useGetDogs } from "./useGetDogs";
 export const Dogs: React.FC = () => {
-  const [response, setResponse] = useState<TResponse>({
-    message: "",
-    status: "loading",
-  });
-  useEffect(() => {
-    setTimeout(() => {
-      getDogs({ setResponse });
-    }, 1250);
-  }, []);
+  const { response, setResponse, getDogs } = useGetDogs();
   return (
     <>
       <Header title="Random Dogs" />
