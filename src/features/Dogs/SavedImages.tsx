bed2cc4679eacec6e407a0b460bc9595241da4cf
Jakub Pacewicz <pecet3107@gmail.coim>
2023-04-09 00:13:34 +0200

    edited component

--- src/features/Dogs/SavedImages.tsx
+++ src/features/Dogs/SavedImages.tsx
@@ -1,16 +1,30 @@
 import Context, { TContext } from "./DogsContext";
 import { useContext } from "react";
 import { nanoid } from "nanoid";
+import { HiOutlineHeart } from "react-icons/hi";
+import { Icon } from "../../common/Icon";
 export const SavedImages = () => {
   const { state, addToArray } = useContext(Context) as TContext;
   return (
-    <>
-      <button onClick={() => addToArray()}>Add to List</button>
-      <div className="grid grid-cols-2">
+    <section className="m-8">
+      <button
+        onClick={() => addToArray()}
+        className="m-auto my-8 flex justify-center gap-2 rounded-md bg-red-400 p-2 text-pink-800"
+      >
+        <HiOutlineHeart size="24" />
+        add to favourite
+      </button>
+      <div className="grid grid-cols-2 justify-center gap-2 text-center md:grid-cols-3 lg:grid-cols-4">
         {state.savedImgs.map((element) => (
-          <img src={element} key={nanoid()} />
+          <button onClick={()=>}>
+            <img
+              src={element}
+              key={nanoid()}
+              className="m-auto h-48 w-64 rounded-lg object-fill md:h-64 lg:w-96"
+            />
+          </button>
         ))}
       </div>
-    </>
+    </section>
   );
 };
