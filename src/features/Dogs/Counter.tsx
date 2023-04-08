6bdd6fcc0138a5622ae5eccd814c6ff0c9c104af
Jakub Pacewicz <pecet3107@gmail.coim>
2023-04-08 21:56:08 +0200

    edited button

--- src/features/Dogs/Counter.tsx
+++ src/features/Dogs/Counter.tsx
@@ -1,10 +1,14 @@
 import Context, { TContext } from "./DogsContext";
 import { useContext } from "react";
+import { nanoid } from "nanoid";
 export const Counter = () => {
-  const { state, add } = useContext(Context) as TContext;
+  const { state, addToArray } = useContext(Context) as TContext;
   return (
     <>
-      <button onClick={() => add()}>+1</button>
+      <button onClick={() => addToArray()}>+1</button>
+      {state.savedImgs.map((element) => (
+        <img src={element} key={nanoid()} />
+      ))}
     </>
   );
 };
