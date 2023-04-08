81703f423829b3fa2d0805967f22eb69bdca9d39
Jakub Pacewicz <pecet3107@gmail.coim>
2023-04-09 00:12:50 +0200

    removed imopoorts

--- src/features/Dogs/Core.tsx
+++ src/features/Dogs/Core.tsx
@@ -1,11 +1,5 @@
 import { RefreshButton } from "../../common/RefreshButton";
-import { Header } from "../../common/Header";
-import { Sidebar } from "../../common/Sidebar";
-import { Error } from "../../common/Error";
-import { Loading } from "../../common/Loading";
-import { MainWrapper } from "../../common/MainWrapper";
 import { ImageAndFilters } from "../../common/ImageAndFilters";
-import { useGetDogs } from "./useGetDogs";
 import Context, { TContext } from "./DogsContext";
 import { useContext } from "react";
 
