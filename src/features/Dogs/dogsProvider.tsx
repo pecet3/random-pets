478d67c0a0c8f9ca66fc9e580bdb2dff447f10ec
Jakub Pacewicz <pecet3107@gmail.coim>
2023-04-08 15:10:56 +0200

    replaced Context

--- src/features/Dogs/dogsProvider.tsx
+++
@@ -1,7 +0,0 @@
-import React, { Component, useState } from "react";
-import DogsContext, { defaultValue } from "./dogsContext";
-
-export const DogsProvider = () => {
-  const [state, setState] = useState(defaultValue);
-  return <DogsContext.Provider value={state}></DogsContext.Provider>;
-};
