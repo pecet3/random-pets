fe4ac0e463c4b4d742a7026013d4aed37b71bd09
Jakub Pacewicz <pecet3107@gmail.coim>
2023-04-08 21:55:57 +0200

    created the component

---
+++ src/features/Dogs/Core.tsx
@@ -0,0 +1,21 @@
+import { RefreshButton } from "../../common/RefreshButton";
+import { Header } from "../../common/Header";
+import { Sidebar } from "../../common/Sidebar";
+import { Error } from "../../common/Error";
+import { Loading } from "../../common/Loading";
+import { MainWrapper } from "../../common/MainWrapper";
+import { ImageAndFilters } from "../../common/ImageAndFilters";
+import { useGetDogs } from "./useGetDogs";
+import Context, { TContext } from "./DogsContext";
+import { Counter } from "./Counter";
+import { useContext } from "react";
+
+export const Core: React.FC = () => {
+  const { state, addToArray, getImage } = useContext(Context) as TContext;
+  return (
+    <>
+      <ImageAndFilters image={state.fetch ? state.fetch.message : ""} />
+      <RefreshButton action={() => getImage()} petType="Dog" />
+    </>
+  );
+};
