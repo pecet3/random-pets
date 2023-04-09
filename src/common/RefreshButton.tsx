23a5b514c46c517df99e7f91f496ec0c47321771
Jakub Pacewicz <pecet3107@gmail.coim>
2023-04-09 13:39:24 +0200

    updated tailwind

--- src/common/RefreshButton.tsx
+++ src/common/RefreshButton.tsx
@@ -12,7 +12,7 @@ export const RefreshButton: React.FC<IRefresh> = ({ action, petType }) => {
   return (
     <div>
       <button
-        className="mt-2 rounded-xl bg-teal-600 px-4 py-2 transition-all duration-300 md:hover:rounded-md md:hover:bg-teal-700"
+        className="mt-0 rounded-xl bg-teal-600 px-4 py-2 transition-all duration-300 md:hover:rounded-md md:hover:bg-teal-700"
         onClick={onClickButton}
       >
         <div className="flex items-center gap-2 font-bebas text-lg tracking-wide text-lime-300">
