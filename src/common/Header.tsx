045687d4caa9a3e76b89df18b6718e7bf081c5fb
Jakub Pacewicz <pecet3107@gmail.coim>
2023-04-09 13:39:19 +0200

    updated tailwind

--- src/common/Header.tsx
+++ src/common/Header.tsx
@@ -8,13 +8,12 @@ export const Header: React.FC<IHeader> = ({ title }) => {
   return (
     <header
       className="bg-[rgba(255,255,255,0.6)] 
-    p-2 pb-4 font-bold 
-    dark:bg-transparent md:p-4 md:font-normal lg:bg-transparent dark:lg:bg-transparent"
+    p-2 pb-4 dark:bg-transparent md:p-4 lg:bg-transparent dark:lg:bg-transparent"
     >
       <h1
-        className="font-comics text-4xl tracking-widest
-       text-violet-700 dark:text-pink-500 lg:text-5xl
-        lg:text-green-700 dark:lg:text-pink-400"
+        className="font-comics text-4xl font-bold
+       tracking-widest text-violet-700 dark:text-pink-500
+         lg:text-5xl dark:lg:text-pink-400"
       >
         {title}
       </h1>
