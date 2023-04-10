79a19133026ea79f953d7f765c25bba71df4c979
Jakub Pacewicz <pecet3107@gmail.coim>
2023-04-10 16:22:23 +0200

    removed button, implemented component

--- src/features/Dogs/SavedImages.tsx
+++ src/features/Dogs/SavedImages.tsx
@@ -1,7 +1,6 @@
 import Context, { TContext } from "./DogsContext";
 import { useContext } from "react";
 import { nanoid } from "nanoid";
-import { HiOutlineHeart } from "react-icons/hi";
 import { TiDelete } from "react-icons/ti";
 export const SavedImages = () => {
   const { state, addToArray, setFetchMessage } = useContext(
@@ -9,13 +8,6 @@ export const SavedImages = () => {
   ) as TContext;
   return (
     <section className="m-2">
-      <button
-        onClick={() => addToArray()}
-        className="m-auto mb-8 flex justify-center gap-2 rounded-md bg-red-400 p-2 text-pink-800"
-      >
-        <HiOutlineHeart size="24" />
-        add to favourite
-      </button>
       <div className="grid grid-cols-2 justify-center gap-2 text-center md:grid-cols-3 lg:grid-cols-4 ">
         {state.savedImgs.map((element, index) => (
           <div key={nanoid()} className="relative">
