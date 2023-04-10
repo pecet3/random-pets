5e66a36bbcedd748983b4b0d7d6f89ea6ac13960
Jakub Pacewicz <pecet3107@gmail.coim>
2023-04-10 15:24:55 +0200

    created delete button

--- src/features/Dogs/SavedImages.tsx
+++ src/features/Dogs/SavedImages.tsx
@@ -2,7 +2,7 @@ import Context, { TContext } from "./DogsContext";
 import { useContext } from "react";
 import { nanoid } from "nanoid";
 import { HiOutlineHeart } from "react-icons/hi";
-import { Icon } from "../../common/Icon";
+import { TiDeleteOutline } from "react-icons/ti";
 export const SavedImages = () => {
   const { state, addToArray, setFetchMessage } = useContext(
     Context
@@ -18,12 +18,20 @@ export const SavedImages = () => {
       </button>
       <div className="grid grid-cols-2 justify-center gap-2 text-center md:grid-cols-3 lg:grid-cols-4 ">
         {state.savedImgs.map((element, index) => (
-          <button key={nanoid()} onClick={() => setFetchMessage(index)}>
-            <img
-              src={element}
-              className="m-auto h-48 w-64 rounded-lg object-fill md:h-64 lg:w-96"
-            />
-          </button>
+          <div key={nanoid()} className="">
+            <button onClick={() => setFetchMessage(index)}>
+              <img
+                src={element}
+                className="m-auto h-48 w-64 rounded-lg object-fill md:h-64 lg:w-96"
+              />
+            </button>
+            <button
+              className="relative right-96 bottom-56 text-2xl text-red-600"
+              onClick={() => alert("aaaa")}
+            >
+              <TiDeleteOutline size="32" />
+            </button>
+          </div>
         ))}
       </div>
     </section>
