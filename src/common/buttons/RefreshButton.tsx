0bc3e1cbd080ac130ae02a26c20459db41f92768
Jakub Pacewicz <pecet3107@gmail.coim>
2023-04-10 16:22:02 +0200

    removed div

--- src/common/buttons/RefreshButton.tsx
+++ src/common/buttons/RefreshButton.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { Icon } from "../Icon";
 import { TbRefresh } from "react-icons/tb";
 interface IRefresh {
   action: () => void;
@@ -10,16 +9,14 @@ export const RefreshButton: React.FC<IRefresh> = ({ action, petType }) => {
     action();
   };
   return (
-    <div>
-      <button
-        className="mt-0 rounded-xl bg-teal-600 px-4 py-2 transition-all duration-300 md:hover:rounded-md md:hover:bg-teal-700"
-        onClick={onClickButton}
-      >
-        <div className="flex items-center gap-2 font-bebas text-lg tracking-wide text-lime-300">
-          <Icon icon={<TbRefresh size="24" />} classes="mx-auto" />
-          <p>New {petType}</p>
-        </div>
-      </button>
-    </div>
+    <button
+      className="m-0 h-12 rounded-xl bg-teal-600 px-4 py-2 transition-all duration-300 md:hover:rounded-md md:hover:bg-teal-700"
+      onClick={onClickButton}
+    >
+      <div className="flex items-center gap-2 font-bebas text-lg tracking-wide text-lime-300">
+        <TbRefresh size="24" />
+        <p>New {petType}</p>
+      </div>
+    </button>
   );
 };
