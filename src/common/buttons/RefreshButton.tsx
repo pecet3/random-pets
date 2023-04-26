deeccc1c0fb5600f45f584c5f6a04caccf92af38
Jakub Pacewicz <pecet3107@gmail.coim>
2023-04-26 18:45:02 +0200

    removed handle

--- src/common/buttons/RefreshButton.tsx
+++ src/common/buttons/RefreshButton.tsx
@@ -5,13 +5,10 @@ interface IRefresh {
   petType?: string;
 }
 export const RefreshButton: React.FC<IRefresh> = ({ action, petType }) => {
-  const onClickButton = () => {
-    action();
-  };
   return (
     <button
       className="m-0 rounded-xl bg-teal-600 px-4 py-2 transition-all duration-300 md:hover:rounded-md md:hover:bg-teal-700"
-      onClick={onClickButton}
+      onClick={() => action()}
     >
       <div className="flex items-center gap-2 font-bebas text-lg tracking-wide text-lime-300">
         <TbRefresh size="24" />
