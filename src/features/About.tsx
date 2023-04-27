589ed5b2c99c21755e22dfbe82278fc4055e5917
Jakub Pacewicz <pecet3107@gmail.coim>
2023-04-27 21:07:46 +0200

    Implemented Header

--- src/features/About.tsx
+++ src/features/About.tsx
@@ -1,7 +1,9 @@
 import { Sidebar } from "../common/Sidebar";
+import { Header } from "../common/Header";
 export const About: React.FC = () => {
   return (
     <>
+      <Header />
       <Sidebar />
       <section className="mt-20">
         <p>Hello, this is About section</p>
