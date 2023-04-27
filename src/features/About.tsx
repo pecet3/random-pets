a0b94ac11e9e604b7867dd0ebbd3ca81b541b878
Jakub Pacewicz <pecet3107@gmail.coim>
2023-04-27 21:04:15 +0200

    implemented Sidebar

--- src/features/About.tsx
+++ src/features/About.tsx
@@ -1,7 +1,11 @@
+import { Sidebar } from "../common/Sidebar";
 export const About: React.FC = () => {
   return (
-    <section>
-      <p>Hello, this is About section</p>
-    </section>
+    <>
+      <Sidebar />
+      <section className="mt-20">
+        <p>Hello, this is About section</p>
+      </section>
+    </>
   );
 };
