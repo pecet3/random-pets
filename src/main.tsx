8c4dcbd5ef54805473b51007785dd8e92413c6b8
Jakub Pacewicz <pecet3107@gmail.coim>
2023-04-28 11:53:32 +0200

    removed Provider and store

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { createHashRouter } from "react-router-dom";
 import { About } from "./features/About";
-import { Provider } from "react-redux";
 import { Dogs } from "./features/Dogs";
 import { Cats } from "./features/Cats";
-import store from "./store";
 import App from "./App";
 import "./index.css";
 
@@ -30,8 +28,6 @@ export const router = createHashRouter([
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <App />
   </React.StrictMode>
 );
