05755b4f5b95068dae845f65166b3ac903e794bf
Jakub Pacewicz <pecet3107@gmail.coim>
2023-04-28 12:12:51 +0200

    edited pathj in impotrt

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import { createHashRouter } from "react-router-dom";
 import { About } from "./features/About";
 import { Dogs } from "./features/Dogs";
-import { Cats } from "./features/Cats";
+import { Cats } from "./features/Cats/Cats";
 import App from "./App";
 import "./index.css";
 
