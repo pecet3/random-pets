adce9bb2c3d40d2719061b5d09b2abd381831ae8
Jakub Pacewicz <pecet3107@gmail.coim>
2023-04-28 12:15:27 +0200

    implemented MainWrapper

--- src/features/About.tsx
+++ src/features/About.tsx
@@ -1,16 +1,17 @@
 import { Sidebar } from "../common/Sidebar";
 import { Header } from "../common/Header";
+import { MainWrapper } from "../common/MainWrapper";
 export const About: React.FC = () => {
   return (
     <>
       <Header />
       <Sidebar />
-      <section className="mt-20">
-        <p>
-          Welcome to site with random photos of Dogs and Cats. If you like some
-          photo, you can add it to favourite list. Enjoy!
+      <MainWrapper>
+        <p className="m-auto max-w-xl rounded-2xl p-12 pb-28 text-xl font-bold text-emerald-800 backdrop-blur">
+          Welcome to the site with random photos of Dogs and Cats. If you like
+          some photo, you can add it to the favourite list. Enjoy!
         </p>
-      </section>
+      </MainWrapper>
     </>
   );
 };
